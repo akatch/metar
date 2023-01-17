650bd0b6c7b562b6a28d036cc77c2530584c50af
Al Bowles <bowlesalx@gmail.com>
2023-01-16 21:31:14 -0600

    Implement Metar.parse(string) and add associated tests

--- lib/metar.ex
+++ lib/metar.ex
@@ -3,16 +3,156 @@ defmodule Metar do
   Documentation for `Metar`.
   """
 
+  defp c_to_int(""), do: 0
+  defp c_to_int("M" <> str), do: -c_to_int(str)
+  defp c_to_int(str), do: String.to_integer(str)
+  defp kt_to_int(""), do: nil
+  defp kt_to_int(str), do: String.to_integer(str)
+
+  defp translate_condition(c) do
+    case c do
+      c when c in ["CLR", "SKC"] -> "clear"
+      "FEW" -> "partly cloudy"
+      "SCT" -> "cloudy"
+      "BKN" -> "mostly cloudy"
+      "OVC" -> "overcast"
+      "VV" -> "vertical visibility"
+      "" -> false
+    end
+  end
+
+  defp translate_quality(quality) do
+    case quality do
+      "+" -> "heavy"
+      "-" -> "light"
+      "VC" -> "vicinity"
+      "" -> nil
+    end
+  end
+
+  defp translate_other(other) do
+    case other do
+      "SQ" -> "squall"
+      "SS" -> "sandstorm"
+      "FC" -> "tornado"
+      "DS" -> "dust storm"
+      "PO" -> "sand whirls"
+      "" -> nil
+    end
+  end
+
+  defp translate_description(description) do
+    case description do
+      "MI" -> "shallow"
+      "BL" -> "blowing"
+      "BC" -> "patchy"
+      "SH" -> "showers"
+      "PR" -> "partial"
+      "DR" -> "drifting"
+      "TS" -> "thunderstorms"
+      "FZ" -> "freezing"
+      "" -> nil
+    end
+  end
+
+  defp translate_precipitation(precipitation) do
+    case precipitation do
+      "DZ" -> "drizzle"
+      "IC" -> "ice crystals"
+      "UP" -> "unknown"
+      "RA" -> "rain"
+      "PL" -> "ice pellets"
+      "SN" -> "snow"
+      "GR" -> "hail"
+      "SG" -> "snow grains"
+      "GS" -> "small hail"
+      "" -> nil
+    end
+  end
+
+  defp translate_obscurity(obscurity) do
+    case obscurity do
+      "BR" -> "mist"
+      "SA" -> "sand"
+      "FU" -> "smoke"
+      "FG" -> "fog"
+      "HZ" -> "haze"
+      "VA" -> "volcanic ash"
+      "PY" -> "spray"
+      "DU" -> "dust"
+      "" -> nil
+    end
+  end
+
+  defp translate_phenomena(quality, description, precipitation, obscurity, other) do
+    phenomena =
+      Enum.reject(
+        [
+          translate_quality(quality),
+          translate_description(description),
+          translate_precipitation(precipitation),
+          translate_obscurity(obscurity),
+          translate_other(other)
+        ],
+        &is_nil/1
+      )
+
+    cond do
+      length(phenomena) > 0 ->
+        Enum.join(phenomena, " ")
+
+      true ->
+        nil
+    end
+  end
+
   @doc """
-  Hello world.
+  Parse a METAR string.
 
   ## Examples
 
-      iex> Metar.hello()
-      :world
+  iex> Metar.parse("2023/01/13 15:53 KMSN 131553Z 36010G18KT 10SM OVC029 M03/M08 A3030 RMK AO2 SLP273 T10281083")
+  %{
+    condition: "overcast",
+    dewpoint_c: -8,
+    phenomena: nil,
+    temperature_c: -3,
+    visibility_mi: 10,
+    wind_bearing: 360,
+    wind_gusting_kt: 18,
+    wind_speed_kt: 10
+  }
 
   """
-  def hello do
-    :world
+  def parse(metar) do
+    %{
+      "condition" => c,
+      "dewpoint" => dp,
+      "gusting" => g,
+      "quality" => qual,
+      "description" => desc,
+      "precipitation" => prec,
+      "obscurity" => obs,
+      "other" => oth,
+      "temperature" => t,
+      "visibility" => v,
+      "wind_speed" => ws,
+      "wind_direction" => wd
+    } =
+      Regex.named_captures(
+        ~r/(?<wind_direction>\d{3})(?<wind_speed>\d{2})(?:G(?<gusting>\d{2}))?KT\s(?<visibility>\d+)(?:SM)?(?:\s(?<quality>\+|-|VC)?(?<description>MI|BL|BC|SH|PR|DR|TS|FZ)?(?<precipitation>DZ|IC|UP|RA|PL|SN|GR|SG|GS)?(?<obscurity>BR|SA|FU|HZ|VA|PY|DU|FG)?(?<other>SQ|FC|SS|DS|PO)?)?\s(?<condition>CLR|SKC|FEW|SCT|BKN|OVC|VV)(?:\d{3})?(?:.*)?\s(?<temperature>M?(\d{2}))\/(?<dewpoint>M?(\d{2}))?/,
+        metar
+      )
+
+    %{
+      condition: translate_condition(c),
+      dewpoint_c: c_to_int(dp),
+      phenomena: translate_phenomena(qual, desc, prec, obs, oth),
+      temperature_c: c_to_int(t),
+      visibility_mi: String.to_integer(v),
+      wind_bearing: String.to_integer(wd),
+      wind_gusting_kt: kt_to_int(g),
+      wind_speed_kt: kt_to_int(ws)
+    }
   end
 end
--- test/metar_test.exs
+++ test/metar_test.exs
@@ -2,7 +2,125 @@ defmodule MetarTest do
   use ExUnit.Case
   doctest Metar
 
-  test "greets the world" do
-    assert Metar.hello() == :world
+  @cases [
+    {
+      "2023/01/13 15:53\nKMSN 131553Z 36010G18KT 10SM OVC029 M03/M08 A3030 RMK AO2 SLP273 T10281083\n",
+      %{
+        condition: "overcast",
+        dewpoint_c: -8,
+        phenomena: nil,
+        temperature_c: -3,
+        visibility_mi: 10,
+        wind_bearing: 360,
+        wind_gusting_kt: 18,
+        wind_speed_kt: 10
+      },
+      %{
+        heat_index_c: nil,
+        relative_humidity: 68,
+        summary: "27°F (16°F) overcast",
+        temperature_f: 27,
+        temperature_k: 267,
+        wind_chill_c: -9,
+        wind_speed_kph: 19,
+        wind_speed_mph: 12
+      }
+    },
+    {
+      "2023/01/09 15:55\nKRYV 091555Z AUTO 22006KT 7SM CLR M02/M04 A3009 RMK AO2 T10211045\n",
+      %{
+        condition: "clear",
+        dewpoint_c: -4,
+        phenomena: nil,
+        temperature_c: -2,
+        visibility_mi: 7,
+        wind_bearing: 220,
+        wind_gusting_kt: nil,
+        wind_speed_kt: 6
+      },
+      %{
+        heat_index_c: nil,
+        relative_humidity: 86,
+        summary: "28°F (21°F) clear",
+        temperature_f: 28,
+        temperature_k: 268,
+        wind_chill_c: -6,
+        wind_speed_kph: 11,
+        wind_speed_mph: 7
+      }
+    },
+    {
+      "2023/01/12 01:26\nKMDW 120126Z 19003KT 8SM BKN021 OVC033 09/06 A2980 RMK AO2 T00890056\n",
+      %{
+        condition: "mostly cloudy",
+        dewpoint_c: 6,
+        phenomena: nil,
+        temperature_c: 9,
+        visibility_mi: 8,
+        wind_bearing: 190,
+        wind_gusting_kt: nil,
+        wind_speed_kt: 3
+      },
+      %{
+        heat_index_c: nil,
+        relative_humidity: 81,
+        summary: "48°F (47°F) mostly cloudy",
+        temperature_f: 48,
+        temperature_k: 279,
+        wind_chill_c: 8,
+        wind_speed_kph: 6,
+        wind_speed_mph: 3
+      }
+    },
+    {
+      "2023/01/12 00:56\nKHNB 120056Z AUTO 00000KT 4SM BR BKN036 12/12 A2984 RMK AO2 SLP105 T01170117\n",
+      %{
+        condition: "mostly cloudy",
+        dewpoint_c: 12,
+        phenomena: "mist",
+        temperature_c: 12,
+        visibility_mi: 4,
+        wind_bearing: 0,
+        wind_gusting_kt: nil,
+        wind_speed_kt: 0
+      },
+      %{
+        heat_index_c: nil,
+        relative_humidity: 100,
+        summary: "54°F mist",
+        temperature_f: 54,
+        temperature_k: 282,
+        wind_chill_c: nil,
+        wind_speed_kph: 0,
+        wind_speed_mph: 0
+      }
+    },
+    {
+      "2023/01/10 20:53\nKDFW 102053Z 21018KT 10SM BKN250 28/07 A2983 RMK AO2 PK WND 22026/2011 SLP095 T02830067 56036\n",
+      %{
+        condition: "mostly cloudy",
+        dewpoint_c: 7,
+        phenomena: nil,
+        temperature_c: 28,
+        visibility_mi: 10,
+        wind_bearing: 210,
+        wind_gusting_kt: nil,
+        wind_speed_kt: 18
+      },
+      %{
+        heat_index_c: 27,
+        relative_humidity: 27,
+        summary: "82°F (81°F) mostly cloudy",
+        temperature_f: 82,
+        temperature_k: 298,
+        wind_chill_c: nil,
+        wind_speed_kph: 33,
+        wind_speed_mph: 21
+      }
+    }
+  ]
+
+  test "METAR parsing" do
+    for {input, output, _converted} <- @cases, do: assert(output == Metar.parse(input))
   end
 end
